cd1af939059139afe9fdd330f4a485361e5a7c18
Bightheory <humpheryeze20@gmail.com>
2022-09-09 17:10:25 -0700

    Task 9

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
@@ -31,4 +31,4 @@ int main(void)
 
 	/* return description */
 	return (0);
-}        
+}
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-  * main -Entry point
-  *
-  * Return Always (success)
-  */
+ * main - Entry point
+ *
+ * Return Always (success)
+ */
 int main(void)
 {
 	char i;
