7fbae2d71afa4a7bc55dd954762053c1988a1bb5
Bightheory <humpheryeze20@gmail.com>
2022-09-19 15:25:34 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * int _strlen - returns the length of a string
+  * @s: parameter
+  * Return: int
+  */
+
+int _strlen(char*s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
