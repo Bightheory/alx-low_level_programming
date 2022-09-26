02ff8db4b950fc6d439025e9e88c314dac65f18e
Bightheory <humpheryeze20@gmail.com>
2022-09-26 14:03:56 -0700

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,18 +5,20 @@
   * @n: bytes of the memory
   * @src: memory to be copied from
   * @dest: memory to be copied to
-  * Return: pointer to dest
+  * Return: dest
   */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
+	/*Delacring WHILE*/
 	while (i < n)
 	{
 		*(dest + i) = *(src + i); /*add 1 position dest and src*/
 		i++;
 	}
+	/*END WHILE*/
 
 	return (dest);
 }
