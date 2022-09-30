e11a606b1df9bfdd892f29e6093b2e4466394ba5
Bightheory <humpheryeze20@gmail.com>
2022-09-30 09:49:53 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *main - Prints it's name followed by a new line
+  *
+  *Return: (0)
+  */
+
+int main(int argc, char *argv[])
+{
+	char name[] = "./mynameis";
+	int i;
+
+
+	for (i = 0; i > 0; i++)
+	{
+		printf("%s\n", name);
+	}
+	return (0);
+}
