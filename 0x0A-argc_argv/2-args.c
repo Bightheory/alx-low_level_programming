881839570e51438867bf53767140e5ea0518f836
Bightheory <humpheryeze20@gmail.com>
2022-09-30 11:06:13 -0700

    Task 1

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  *main -  prints all arguments it receives.
+  *@argc: argument count
+  *@argv: argument
+  *Return: (0)
+  */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
