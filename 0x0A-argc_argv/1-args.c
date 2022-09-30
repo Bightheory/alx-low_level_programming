4cd14fda20e0e04d32f84b88d3f91689fd69b7b2
Bightheory <humpheryeze20@gmail.com>
2022-09-30 10:40:55 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  *main - prints the number of arguments passed into it
+  *@argc: argument count
+  *@argv: argument
+  *Return: (0)
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
