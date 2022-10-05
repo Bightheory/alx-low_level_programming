7ce25b6b38fc5025f9aee0764118b95690d619bc
Bightheory <humpheryeze20@gmail.com>
2022-10-05 12:23:28 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		S2 = "";
+		s2 = "";
 	while (*(s1 + i))
 		len1++, i++;
 	while (*(s2 + j))
@@ -29,7 +29,7 @@ char *str_concat(char *s1, char *s2)
 	concat = malloc(sizeof(char) * (len1 + len2)); /*alloc memory*/
 
 	if (concat == NULL) /* validate memory */
-			       return (NULL);
+		return (NULL);
 
 	i = 0, j = 0;
 	while (i < len1) /* concatenate */
