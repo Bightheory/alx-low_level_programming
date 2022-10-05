778685843fb41b8e653b261ac1fe301d389d0706
Bightheory <humpheryeze20@gmail.com>
2022-10-05 12:04:51 -0700

    Task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,69 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- *_strlen - count array
- *@s: array of elements
- *Return: 1
- */
+  * _strdup - returns a pointer to a newly allocated space in memory,
+  * which contains a copy of the string given as a parameter`
+  * @str: string to duplicate
+  *Return: pointer to duplicated string in allocated memory
+  */
 
-int _strlen(char *s)
-{
-	unsigned int i;
-
-	i = 0;
-	while (s[i] != '\0') /*count character of string*/
-	{
-		i++;
-	}
-
-	return (i);
-}
-
-
-/**
- *_strcpy - copy arrays
- *@src: array of elements
- *@dest: dest array
- *Return: dest
- */
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-
-	dest[i] = '\0';
-
-	return (dest);
-}
-
-/**
- *_strdup - array for prints a string
- *@str: array of elements
- *Return: pointer
- */
 char *_strdup(char *str)
 {
-	char *dst;
-	unsigned int size;
+	char *duplicate_str;
+	int i = 0, len = 0;
 
-	if (str == 0)
-	{
+	if (str == NULL) /* validate str input */
 		return (NULL);
+
+	while (*(str + i))
+		len++, i++;
+	len++; /* add null terminator to length */
+
+	duplicate_str = malloc(sizeof(char) * len); /* allocate memory */
+
+	if (duplicate_str == NULL) /* validate memory */
+		return (NULL);
+
+	i = 0;
+	while (i < len)
+	{
+		*(duplicate_str + i) = *(str + i);
+		i++;
 	}
 
-	size = _strlen(str) + 1;
-
-	dst = (char *) malloc(size *sizeof(char));
-
-	if (dst == 0)
-	{
-		return (NULL);
-	}
-	_strcpy(dst, str);
-	return (dst);
-
+	return (duplicate_str);
+}
